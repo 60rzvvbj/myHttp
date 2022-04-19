ace6ac97bbf0db6cdc78586bf559978ade6fb690
60rzvvbj <1808078515@qq.com>
2022-04-19 21:54:44 +0800

    feat: 封装XHR

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,5 @@
 import net from "net";
 function connect(options, data, callback) {
-  // console.log();
   let client = net.createConnection(
     { port: parseInt(options.port), host: options.host },
     () => {
@@ -13,6 +12,59 @@ function connect(options, data, callback) {
   });
 }
 
+function XHR() {
+  let options = {
+    method: "",
+    url: "",
+    host: "",
+    port: "8848",
+    text: "",
+  };
+
+  let flag = 0; // 判断函数调用情况，防止非法调用
+
+  this.open = function (method, url) {
+    options.method = method;
+    options.url = url;
+
+    // url处理
+    options.text += options.method + " " + options.url + " 1.0";
+
+    // myhttp://host:port/path?a=1&b=2
+    let urlArr = url.substring(9).split(":");
+    if (urlArr.length == 1) {
+      options.host = urlArr[0];
+    } else if (urlArr.length == 2) {
+      options.host = urlArr[0];
+      options.port = urlArr[1].split("/")[0];
+    }
+  };
+
+  this.setHeader = function (headers) {
+    for (let attr in headers) {
+      options.text += "\n" + attr + "=" + headers[attr];
+    }
+  };
+
+  this.send = function (bodyData) {
+    if (bodyData != undefined) {
+      options.text += "\n\n";
+      options.text += bodyData;
+    }
+
+    connect(
+      {
+        host: options.host,
+        port: options.port,
+      },
+      options.text,
+      this.callback
+    );
+  };
+
+  this.callback = function () {};
+}
+
 function request(options) {
   let defaultOptions = {
     method: "GET",
@@ -20,17 +72,7 @@ function request(options) {
   };
   Object.assign(defaultOptions, options);
 
-  let url = defaultOptions.url;
-  let text = "";
-  let host, port;
-  let urlArr = url.substring(9).split(":");
-  if (urlArr.length == 1) {
-    host = urlArr[0];
-    port = "8848";
-  } else if (urlArr.length == 2) {
-    host = urlArr[0];
-    port = urlArr[1].split("/")[0];
-  }
+  let xhr = new XHR();
 
   if (defaultOptions.query) {
     let queryArr = [];
@@ -39,28 +81,22 @@ function request(options) {
     }
     defaultOptions.url += "?" + queryArr.join("&");
   }
-  text += defaultOptions.method + " " + defaultOptions.url + " 1.0\n";
-  text += "\n";
-  text += defaultOptions.body;
-  connect(
-    {
-      host,
-      port,
-    },
-    text,
-    (err, resData) => {
-      if (err) {
-        defaultOptions.error(err);
-      } else {
-        defaultOptions.success(resData);
-      }
+
+  xhr.open(defaultOptions.method, defaultOptions.url);
+
+  xhr.callback = function (err, resData) {
+    if (err) {
+      defaultOptions.error(err);
+    } else {
+      defaultOptions.success(resData);
     }
-  );
+  };
+  xhr.send(defaultOptions.body);
 }
 
 request({
   method: "GET",
-  url: "myhttp://192.168.43.205:1234/login",
+  url: "myhttp://192.168.0.168:1234/login",
   query: { account: "60rzvvbj", password: "123456" },
   body: "aoiughadipsugfasdbgfapisdvbgwedbf",
   success: (data) => {
